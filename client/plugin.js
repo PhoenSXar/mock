3b0cff3918baf256a041a19b777b2f3de6b28534
DameNeko <phoensxar@gmail.com>
2018-05-03 09:11:33 +0800

    test

--- client/plugin.js
+++ client/plugin.js
@@ -1,171 +1,40 @@
 let hooks, pluginModule;
 
-/**
- * type component  组件
- *      listener   监听函数
- * mulit 是否绑定多个监听函数
- *
- */
-
 hooks = {
-  /**
-   * 第三方登录 //可参考 yapi-plugin-qsso 插件
-   */
   third_login: {
     type: 'component',
     mulit: false,
     listener: null
   },
-  /**
-   * 导入数据
-   * @param Object importDataModule
-   *
-   * @info
-   * 可参考 vendors/exts/yapi-plugin-import-swagger插件
-   * importDataModule = {};
-   *
-   */
   import_data: {
     type: 'listener',
     mulit: true,
     listener: []
   },
-  /**
-   * 导出数据
-   * @param Object exportDataModule
-   * @param projectId
-   * @info
-   * exportDataModule = {};
-   * exportDataModule.pdf = {
-   *   name: 'Pdf',
-   *   route: '/api/plugin/export/pdf',
-   *   desc: '导出项目接口文档为 pdf 文件'
-   * }
-   */
   export_data: {
     type: 'listener',
     mulit: true,
     listener: []
   },
-  /**
-   * 接口页面 tab 钩子
-   * @param InterfaceTabs
-   *
-   * @info
-   * 可参考 vendors/exts/yapi-plugin-advanced-mock
-   * let InterfaceTabs = {
-      view: {
-        component: View,
-        name: '预览'
-      },
-      edit: {
-        component: Edit,
-        name: '编辑'
-      },
-      run: {
-        component: Run,
-        name: '运行'
-      }
-    }
-   */
+  
   interface_tab: {
     type: 'listener',
     mulit: true,
     listener: []
   },
-  /**
-   * header下拉菜单 menu 钩子
-   * @param HeaderMenu
-   *
-   * @info
-   * 可参考 vendors/exts/yapi-plugin-statistics
-   * let HeaderMenu = {
-  user: {
-    path: '/user/profile',
-    name: '个人中心',
-    icon: 'user',
-    adminFlag: false
-  },
-  star: {
-    path: '/follow',
-    name: '我的关注',
-    icon: 'star-o',
-    adminFlag: false
-  },
-  solution: {
-    path: '/user/list',
-    name: '用户管理',
-    icon: 'solution',
-    adminFlag: true
-
-  },
-  logout: {
-    path: '',
-    name: '退出',
-    icon: 'logout',
-    adminFlag: false
-
-  }
-};
-   */
+  
   header_menu: {
     type: 'listener',
     mulit: true,
     listener: []
   },
-  /**
-   * Route路由列表钩子
-   * @param AppRoute
-   *
-   * @info
-   * 可参考 vendors/exts/yapi-plugin-statistics
-   * 添加位置在Application.js 中
-   * let AppRoute = {
-  home: {
-    path: '/',
-    component: Home
-  },
-  group: {
-    path: '/group',
-    component: Group
-  },
-  project: {
-    path: '/project/:id',
-    component: Project
-  },
-  user: {
-    path: '/user',
-    component: User
-  },
-  follow: {
-    path: '/follow',
-    component: Follows
-  },
-  addProject: {
-    path: '/add-project',
-    component: AddProject
-  },
-  login: {
-    path: '/login',
-    component: Login
-  }
-};
-};
-   */
+  
   app_route: {
     type: 'listener',
     mulit: true,
     listener: []
   },
-  /*
-   * 添加 reducer
-   * @param Object reducerModules
-   * 
-   * @info
-   * importDataModule = {}; 
-   * 
-   */
-
+  
   add_reducer: {
     type: 'listener',
     mulit: true,
--- docs/NAV.md
+++
@@ -1,5 +0,0 @@
-# YApi
-![logo](documents/images/logo_header@2x.png)
-
-
-* [教程](documents/index.md)
